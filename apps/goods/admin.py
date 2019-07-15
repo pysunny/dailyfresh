70b2e405b4fe3146ea62892e02eca648ec7a71a0
sunny <sunnywu@hufcor.com>
2019-07-15 21:53:46 +0800

    add index imformation

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from goods.models import GoodsType
+from goods.models import GoodsType, GoodsSKU, Goods, GoodsImage, IndexGoodsBanner, IndexTypeGoodsBanner, IndexPromotionBanner
 # Register your models here.
+# 商品种类
 admin.site.register(GoodsType)
+# 商品SPK
+admin.site.register(GoodsSKU)
+# 商品SPU
+admin.site.register(Goods)
+# 商品图片
+admin.site.register(GoodsImage)
+# 首页轮播商品
+admin.site.register(IndexGoodsBanner)
+# 首页分类商品
+admin.site.register(IndexTypeGoodsBanner)
+# 首页促销活动
+admin.site.register(IndexPromotionBanner)
--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -171,11 +171,4 @@ SESSION_CACHE_ALIAS = "default"
 # 配置登录url地址
 LOGIN_URL='/user/login' # /accounts/login?next=/user
 
-# 设置Django的文件存储类
-DEFAULT_FILE_STORAGE='utils.fdfs.storage.FDFSStorage'
-
-# 设置fdfs使用的client.conf文件路径
-FDFS_CLIENT_CONF='./utils/fdfs/client.conf'
-
-# 设置fdfs存储服务器上nginx的IP和端口号
-FDFS_URL='http://172.16.179.131:8888/'
+MEDIA_ROOT=os.path.join(BASE_DIR,"static/media")
