dc4b2a981d108d8b1d8cb1f02f249f83312006a8
sunny <sunnywu@hufcor.com>
2019-07-30 16:59:44 +0800

    adding order page 2

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import re_path
-from order.views import OrderPlaceView
+from order.views import OrderPlaceView, OrderCommitView
 app_name = 'order'
 urlpatterns = [
     re_path(r'^place$', OrderPlaceView.as_view(), name='place'), # 购物车添加
+    re_path(r'^commit$', OrderCommitView.as_view(), name='commit'), # 订单创建
 ]
--- apps/order/views.py
+++ apps/order/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
 from goods.models import GoodsSKU
+from order.models import OrderInfo, OrderGoods
+from django.http import JsonResponse
 from user.models import Address
 from django_redis import get_redis_connection
 from utils.mixin import LoginRequiredMixin
+from datetime import datetime
 
 # /order/place
 class OrderPlaceView(LoginRequiredMixin, View):
@@ -64,6 +67,100 @@ class OrderPlaceView(LoginRequiredMixin, View):
                     'sku_ids':sku_ids}
         
         return render(request, 'place_order.html', context)
+
+
+class OrderCommitView(View):
+    def post(self, request):
+        """ 订单创建 """
+        # 验证用户
+        user = request.user
+        if not user.is_authenticated:
+            return JsonResponse({'res':0, 'errmsg':'请登录'})
+
+        # 接收参数
+        addr_id = request.POST.get('addr_id')
+        pay_method = request.POST.get('pay_method')
+        sku_ids = request.POST.get('sku_ids')
+
+        # 验证参数
+        if not all([addr_id, pay_method, sku_ids]):
+            return JsonResponse({'res':1, 'errmsg':'数据不完整'})
+
+        if pay_method not in OrderInfo.PAY_METHODS:
+            return JsonResponse({'res':2, 'errmsg':'非法支付方式'})
+
+        try:
+            addr = Address.objects.get(id=addr_id)
+        except Address.DoesNotExist:
+            return JsonResponse({'res':3, 'errmsg':'非法地址'})
+
+        # todo:创建订单核心业务
+        # 组织参数
+        # 订单id
+        order_id = datetime.now().strftime('%y%m%d%H%M%S')+str(user.id)
+        # 运费
+        transit_price = 10
+        total_count = 0
+        total_price = 0
+
+        order = OrderInfo.objects.create(order_id=order_id,
+                                        user=user,
+                                        addr=addr,
+                                        pay_method=pay_method,
+                                        total_count=total_count,
+                                        total_price=total_price,
+                                        transit_price=transit_price)
+        # todo:创建订单核心业务
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d'%user.id
+        sku_ids = sku_ids.split(',')
+        for sku_id in sku_ids:
+            try:
+                sku = GoodsSKU.objects.get(id=sku_id)
+            except GoodsSKU.DoesNotExist:
+                # 商品不存在
+                return JsonResponse({'res':4, 'errmsg':'商品不存在'})
+
+            count = conn.hget(cart_key, sku_id)
+
+            OrderGoods.objects.create(order=order,
+                                        sku=sku,
+                                        count=count,
+                                        price=sku.price)
+
+            # todo:更新库存
+            sku.stock -= int(count)
+            sku.sales += int(count)
+            sku.save()
+
+            amount = sku.price*int(count)
+            total_count += int(count)
+            total_price += amount
+
+        order.total_count = total_count
+        order.total_price = total_price
+        order.save()
+
+        # 清除用户Redis记录
+        conn.hdel(cart_key, *sku_ids)
+
+        # 返回应答
+        return JsonResponse({'res':5, 'massage':'创建成功'})
+
+
+
+
+
+
+
+
+        
+
+
+
+
+        
+
 
 
 
